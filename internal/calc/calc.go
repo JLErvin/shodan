766cc079eb89414615cdecd797b1e3218546811f
Joshua Ervin <joshue@uw.edu>
2019-09-01 00:13:48 -0700

    Started to refactor keywords

--- internal/calc/calc.go
+++ internal/calc/calc.go
@@ -11,12 +11,28 @@ import (
 const (
 	NEWLINE = "newline"
 	INT     = "int"
+	SIN     = "sin"
+	COS     = "cos"
+	TAN     = "tan"
+	EXIT    = "exit"
+	QUIT    = "quit"
+	LOG     = "log"
+	LN      = "ln"
+	SQRT    = "sqrt"
+	LPAREN  = "("
+	RPAREN  = ")"
+	POW     = "**"
+	SUM     = "+"
+	DIFF    = "-"
+	QUOT    = "/"
+	PROD    = "*"
 )
 
 type Calculator struct {
-	env     map[string]float64
-	queue   *scan.Queue
-	scanner *scan.Scanner
+	env      map[string]float64
+	keywords map[string]bool
+	queue    *scan.Queue
+	scanner  *scan.Scanner
 }
 
 func NewCalculator() *Calculator {
@@ -24,6 +40,16 @@ func NewCalculator() *Calculator {
 	calc.scanner = scan.NewScanner()
 	calc.queue = scan.NewQueue()
 	calc.env = make(map[string]float64)
+	calc.keywords = make(map[string]bool)
+
+	calc.keywords[SIN] = true
+	calc.keywords[COS] = true
+	calc.keywords[TAN] = true
+	calc.keywords[EXIT] = true
+	calc.keywords[QUIT] = true
+	calc.keywords[LOG] = true
+	calc.keywords[LN] = true
+	calc.keywords[SQRT] = true
 
 	calc.env["PI"] = math.Pi
 	calc.env["E"] = math.E
@@ -37,7 +63,7 @@ func (c *Calculator) Run() {
 		cur := c.scanner.NextToken()
 		for cur.String() != NEWLINE {
 			c.queue.Add(cur)
-			if cur.String() == "exit" || cur.String() == "quit" {
+			if cur.String() == EXIT || cur.String() == QUIT {
 				running = false
 			}
 			cur = c.scanner.NextToken()
@@ -70,7 +96,7 @@ func (c *Calculator) evaluate(t *scan.Token) *scan.Token {
 			pretty += key + " = " + strconv.Itoa(int(val)) + "\n"
 		}
 		return scan.NewToken(pretty[:len(pretty)-1], 0)
-	} else if t.String() == "exit" || t.String() == "quit" {
+	} else if t.String() == EXIT || t.String() == QUIT {
 		os.Exit(1)
 		return nil
 	} else {
@@ -80,18 +106,18 @@ func (c *Calculator) evaluate(t *scan.Token) *scan.Token {
 
 func (c *Calculator) expression(t *scan.Token) *scan.Token {
 	peak := c.queue.Peak()
-	if peak.String() == "+" {
-		c.queue.RemoveFront()
-		t1 := c.expression(t)
-		t2 := c.term(c.queue.RemoveFront())
-		sum := t1.GetValue() + t2.GetValue()
-		return scan.NewToken(INT, sum)
-	} else if peak.String() == "-" {
-		c.queue.RemoveFront()
-		t1 := c.expression(t)
-		t2 := c.term(c.queue.RemoveFront())
-		diff := t1.GetValue() - t2.GetValue()
-		return scan.NewToken(INT, diff)
+	if peak.String() == SUM || peak.String() == DIFF {
+		c.filterOne()
+		t1 := c.expression(t).GetValue()
+		t2 := c.term(c.queue.RemoveFront()).GetValue()
+		n := func(t1, t2 float64) float64 {
+			if peak.String() == SUM {
+				return t1 + t2
+			} else {
+				return t1 - t2
+			}
+		}(t1, t2)
+		return scan.NewToken(INT, n)
 	} else {
 		return c.term(t)
 	}
@@ -99,18 +125,18 @@ func (c *Calculator) expression(t *scan.Token) *scan.Token {
 
 func (c *Calculator) term(t *scan.Token) *scan.Token {
 	peak := c.queue.Peak()
-	if peak.String() == "*" {
-		c.queue.RemoveFront()
-		t1 := c.expression(t)
-		t2 := c.term(c.queue.RemoveFront())
-		prod := t1.GetValue() * t2.GetValue()
-		return scan.NewToken(INT, prod)
-	} else if peak.String() == "/" {
-		c.queue.RemoveFront()
-		t1 := c.expression(t)
-		t2 := c.term(c.queue.RemoveFront())
-		quot := t1.GetValue() / t2.GetValue()
-		return scan.NewToken(INT, quot)
+	if peak.String() == PROD || peak.String() == QUOT {
+		c.filterOne()
+		t1 := c.expression(t).GetValue()
+		t2 := c.term(c.queue.RemoveFront()).GetValue()
+		n := func(t1, t2 float64) float64 {
+			if peak.String() == PROD {
+				return t1 * t2
+			} else {
+				return t1 / t2
+			}
+		}(t1, t2)
+		return scan.NewToken(INT, n)
 	} else {
 		return c.power(t)
 	}
@@ -118,7 +144,7 @@ func (c *Calculator) term(t *scan.Token) *scan.Token {
 
 func (c *Calculator) power(t *scan.Token) *scan.Token {
 	peak := c.queue.Peak()
-	if peak.String() == "**" {
+	if peak.String() == POW {
 		c.queue.RemoveFront()
 		t1 := c.factor(t)
 		t2 := c.power(c.queue.RemoveFront())
@@ -134,37 +160,27 @@ func (c *Calculator) factor(t *scan.Token) *scan.Token {
 		return t
 	} else if val, err := c.env[t.String()]; err {
 		return scan.NewToken(INT, val)
-	} else if t.String() == "sqrt" {
+	} else if _, err := c.keywords[t.String()]; err {
 		c.filterOne()
 		t1 := c.expression(c.queue.RemoveFront())
 		c.filterOne()
-		n := math.Sqrt(t1.GetValue())
+		var n float64
+		switch t.String() {
+		case SQRT:
+			n = math.Sqrt(t1.GetValue())
+		case SIN:
+			n = math.Sin(t.GetValue())
+		case COS:
+			n = math.Cos(t.GetValue())
+		case TAN:
+			n = math.Tan(t.GetValue())
+		case LOG:
+			n = math.Log2(t.GetValue())
+		case LN:
+			n = math.Log(t.GetValue())
+		}
 		return scan.NewToken(INT, n)
-	} else if t.String() == "sin" {
-		c.filterOne()
-		t1 := c.expression(c.queue.RemoveFront())
-		c.filterOne()
-		n := math.Sin(t1.GetValue())
-		return scan.NewToken(INT, n)
-	} else if t.String() == "cos" {
-		c.filterOne()
-		t1 := c.expression(c.queue.RemoveFront())
-		c.filterOne()
-		n := math.Cos(t1.GetValue())
-		return scan.NewToken(INT, n)
-	} else if t.String() == "tan" {
-		c.filterOne()
-		t1 := c.expression(c.queue.RemoveFront())
-		c.filterOne()
-		n := math.Tan(t1.GetValue())
-		return scan.NewToken(INT, n)
-	} else if t.String() == "log" {
-		c.filterOne()
-		t1 := c.expression(c.queue.RemoveFront())
-		c.filterOne()
-		n := math.Log(t1.GetValue())
-		return scan.NewToken(INT, n)
-	} else if t.String() == "(" {
+	} else if t.String() == LPAREN {
 		t1 := c.expression(c.queue.RemoveFront())
 		c.filterOne()
 		return t1
